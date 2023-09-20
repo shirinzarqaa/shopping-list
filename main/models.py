9cd176c43f890018802574fb7c39791983d60f0c
shirinzarqaa <shirinzarqaa354@gmail.com>
2023-09-20 15:05:41 +0700

    tutorial 3 done

--- main/models.py
+++ main/models.py
@@ -2,9 +2,11 @@
 
 # Create your models here.
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
     price = models.IntegerField()
     description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
