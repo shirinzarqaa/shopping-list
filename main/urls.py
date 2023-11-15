ccbea6c9540bb2c38fce4ecf275f1b9b8f2593b2
shirinzarqaa <shirinzarqaa354@gmail.com>
2023-11-15 16:25:28 +0700

    done

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import add_product_ajax, get_product_json, show_main, create_product, show_xml,show_json,show_xml_by_id, show_json_by_id, register,login_user, logout_user, edit_product
+from main.views import add_product_ajax, create_product_flutter, get_product_json, show_main, create_product, show_xml,show_json,show_xml_by_id, show_json_by_id, register,login_user, logout_user, edit_product
 from main.views import delete_product
 
 
@@ -19,5 +19,6 @@ urlpatterns = [
     path('edit-product/<int:id>', edit_product, name='edit_product'),
     path('delete/<int:id>', delete_product, name='delete_product'),
     path('get-product/', get_product_json, name='get_product_json'),
-    path('create-product-ajax/', add_product_ajax, name='add_product_ajax')
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
